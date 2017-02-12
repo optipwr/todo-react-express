da9983443822b26d2c1afd5a10e5cfd31511c300
Michael Irby <michael.irbstoomuch@gmail.com>
2017-02-12 17:32:14 -0500

    init commit

---
+++ back-end/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'x',
+	password: 'x',
+	database: 'students'
+});
+
+connection.connect();
+
+/* GET home page. */
+router.get('/getStudents', function(req, res, next) {
+
+	connection.query('SELECT * FROM students', (error, results, fields)=>{
+		if(error) throw error;
+		res.json(results);
+	});
+	// res.json(students);
+});
+
+router.post('/addStudent', (req, res, next)=>{
+	var studentToAdd = req.body.name;
+	connection.query('INSERT INTO students (name) VALUES (?)', [studentToAdd], (error, results, fields)=>{
+		if(error) throw error;
+		connection.query('SELECT * FROM students', (error2, results, fields)=>{
+			if(error2) throw error2;
+			res.json(results);
+		});
+	})
+});
+
+module.exports = router;
